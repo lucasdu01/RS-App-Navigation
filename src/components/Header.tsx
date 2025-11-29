9ed5b6145c421a28d8b4f9adb14571bccfbc8d04
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-11-29 01:40:30 -0300

    implementa tela Home e Product

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { View } from "react-native"
+
+export function Header({ children }: {children: React.ReactNode}) {
+    return(
+        <View 
+            style={{
+                width: "100%",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: 54
+            }}
+        >
+            {children}
+        </View>
+    )
+}
