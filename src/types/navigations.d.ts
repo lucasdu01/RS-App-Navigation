a1fe09c8ecfbc694c458b57bf5accc0b9e2a142f
Lucas Rocha <lucas.lgr.01@gmail.com>
2025-12-02 23:48:02 -0300

    tipando navigation

---
+++ src/types/navigations.d.ts
@@ -0,0 +1,11 @@
+export type RootStackParamList = {
+    home: undefined
+    product: undefined
+}
+
+declare global {
+    namespace ReactNavigation {
+        interface RootParamList extends
+        RootStackParamList {}
+    }
+}
